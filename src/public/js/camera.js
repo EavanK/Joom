48ddb1add260e5f09867d05d7ed700d5cfc4124c
EavanK <eavan.hsk@gmail.com>
2022-02-06 05:01:33 -0800

    fix a bug
    -fix a bug that turn audio/vedio on home page
    -add button to join video call
    -add select option for cameras

--- src/public/js/camera.js
+++ src/public/js/camera.js
@@ -3,42 +3,82 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const join = document.getElementById("join");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let joinVideo = false;
+
+function hidden(hidden) {
+  myFace.hidden = hidden;
+  muteBtn.hidden = hidden;
+  cameraBtn.hidden = hidden;
+  camerasSelect.hidden = hidden;
+}
+
+hidden(true);
 
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
-    cameras.forEach((camera) => {
-      const option = document.createElement("option");
-      option.value = camera.deviceId;
-      option.innerText = camera.label;
-      camerasSelect.appendChild(option);
-    });
+    const currentCamera = myStream.getVideoTracks()[0];
+    if (!document.querySelector("option")) {
+      cameras.forEach((camera) => {
+        const option = document.createElement("option");
+        option.value = camera.deviceId;
+        option.innerText = camera.label;
+        if (currentCamera.label === camera.label) option.selected = true;
+        camerasSelect.appendChild(option);
+      });
+    }
   } catch (e) {
     console.log(e);
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) await getCameras();
   } catch (e) {
     console.log(e);
   }
 }
+
+function handleJoin() {
+  if (!joinVideo) {
+    join.innerText = "Leave Video Call";
+    hidden(false);
+    joinVideo = true;
+    getMedia();
+  } else {
+    join.innerText = "Join Video Call";
+    hidden(true);
+    handleMuteClick();
+    handleCameraClick();
+    joinVideo = false;
+  }
+}
+
 function handleMuteClick() {
   myStream
     .getAudioTracks()
     .forEach((track) => (track.enabled = !track.enabled));
+
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -47,10 +87,12 @@ function handleMuteClick() {
     muted = false;
   }
 }
+
 function handleCameraClick() {
   myStream
     .getVideoTracks()
     .forEach((track) => (track.enabled = !track.enabled));
+
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
@@ -60,6 +102,11 @@ function handleCameraClick() {
   }
 }
 
-getMedia();
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
+join.addEventListener("click", handleJoin);
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("click", handleCameraChange);
