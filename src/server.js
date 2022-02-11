8c9c54b11a1573862287860b64c757994c84e9a0
EavanK <eavan.hsk@gmail.com>
2022-02-11 02:24:21 -0800

    fix errors
    by adding type: module in package.json
    by modifying ES module scope (using import instead of require)
    by importing path, url for __dirname

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 import http from "http";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
